1e1a1d4ddf921b573d4d3ccf29f93d4f5dfa5965
Chaitanya1223456 <kasuchaitanya34@gmail.com>
2024-07-25 10:59:34 +0530

    third-commit

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,50 +1,25 @@
 from django.shortcuts import render
-from django.contrib import messages
-import requests 
-import datetime 
+import json
+import urllib.request
 
 # Create your views here.
-def home(request):
-    
-    if 'city' in request.POST:
-        city = request.POST['city']
+def index(request):
+    if request.method=='POST':
+        city=request.POST['city']
+        res=urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=4d58ba085e38def16310f2b6510550e3').read()
+        json_data=json.loads(res)    
+        data={
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon'])+str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'k',
+            "pressure":str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity'])
+        }
     else:
-        city = 'indore'
-    
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=8b53bbc2f5feaa836b10c688a2ae484a'
-    PARAMS = {'units':'metric'}
-    
-    API_KEY = 'AIzaSyBTqItpOoW4BXJ1EGcoRRJlwiOI4cyYyNI'
-    
-    SEARCH_ENGINE_ID = '24c76df0c21ff4edf'
-    
-    query = city + " 1920x1080"
-    page = 1
-    start = (page - 1) * 10 + 1
-    searchType = 'image'
-    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+        city=''
+        data={}
+    return render(request, 'index.html', {'city': city, 'data':data})
 
-    data = requests.get(city_url).json()
-    count = 1
-    search_items = data.get("items")
-    image_url = search_items[1]['link']
-    
-    
-    try:
-        data = requests.get(url,PARAMS).json()
-        description = data['weather'][0]['description']    
-        icon = data['weather'][0]['icon']
-        temp = data['main']['temp']
-        
-        day = datetime.date.today()
-            
-        return render(request,'./index.html',{'description':description,'icon':icon,'temp':temp,'day':day,'city':city,'exception_occured':False,'image_url':image_url})
-    except TypeError:
-        exception_occured = True
-        messages.error(request,'data not avaiable to API')
-        day = datetime.date.today()
-        
-        return render(request,'./index.html',{'description':'clear sky','icon':'01d','temp':25,'day':day,'city':'indore','exception_occured':True})
         
         
         
--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'weatherproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'static')],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,18 +110,13 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+STATIC_URL = 'static/'
 
 
 # Default primary key field type
